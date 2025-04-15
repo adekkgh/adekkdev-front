0896b2b08d70b43f1c5ebc7f7b62c5c9234aacc8
GioevArsen <gioev.arsen15@list.ru>
2025-04-15 17:19:44 +0300

    feat/typing animation for main page

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
 	css: {
 		preprocessorOptions: {
 			scss: {
-				additionalData: `@use "@/styles/variables.scss" as *;`,
+				additionalData: `@use "@/styles/variables.scss" as *;
+								@use "@/styles/mixins.scss" as *;`,
 			},
 		},
 	},
